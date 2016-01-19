6191729bd9084f1b24eacf9fc6de1febef58f503
Team1557 <team1557@gmail.com>
2016-01-18 20:31:22 -0500

    Drive Code
    Driving works

--- src/org/usfirst/frc/team1557/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team1557/robot/subsystems/DriveSubsystem.java
@@ -3,6 +3,9 @@ package org.usfirst.frc.team1557.robot.subsystems;
 import org.usfirst.frc.team1557.robot.commands.TankDriveCommand;
 
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -13,12 +16,15 @@ import org.usfirst.frc.team1557.robot.OI;
 /**
  *
  */
-public class DriveSubsystem extends Subsystem {
+public class DriveSubsystem extends Subsystem{
 	CANTalon moterOne = new CANTalon(driveMotorOne_ID);
 	CANTalon moterTwo = new CANTalon(driveMotorTwo_ID);
 	CANTalon moterThree = new CANTalon(driveMotorThree_ID);
 	CANTalon moterFour = new CANTalon(driveMotorFour_ID);
 
+	public DriveSubsystem(){
+	
+	}
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 
@@ -26,13 +32,15 @@ public class DriveSubsystem extends Subsystem {
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
 		setDefaultCommand(new TankDriveCommand());
+		
 	}
 
 	public void tankDrive(double x, double y) {
-		moterOne.set(x);
-		moterTwo.set(y);
+		moterOne.set(-x);
+		moterTwo.set(-y);
 		moterThree.set(x);
 		moterFour.set(y);
 	}
+
 
 }
--- src/org/usfirst/frc/team1557/robot/subsystems/IntakeSubsystem.java
+++ src/org/usfirst/frc/team1557/robot/subsystems/IntakeSubsystem.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc.team1557.robot.subsystems;
 
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import static org.usfirst.frc.team1557.robot.RobotMap.*;
@@ -14,37 +16,44 @@ public class IntakeSubsystem extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 	boolean manualOverride = false;
-	//TalonSRX rotateMotor = new TalonSRX(rotateMotorOne_ID);
-	//TalonSRX rotateMotor2 = new TalonSRX(rotateMotorTwo_ID);
-	//TalonSRX intakeMotor = new TalonSRX(intakeMotorOne_ID);
-	Encoder rotateEncoder = new Encoder(rotateEncoderA_ID, rotateEncoderB_ID, true, EncodingType.k2X);
-
+	/**
+	 * disable this talon durings tests until talon exists in the real world.
+	 */
+	//CANTalon rotateMotor = new CANTalon(rotateMotorOne_ID);
+	//CANTalon rotateMotor2 = new TalonSRX(rotateMotorTwo_ID);
+	//CANTalon intakeMotor = new TalonSRX(intakeMotorOne_ID);
+	//Encoder rotateEncoder = new Encoder(rotateEncoderA_ID, rotateEncoderB_ID, true, EncodingType.k2X);
+//	PIDController rotateController = new PIDController(0.33, 0.33, 0.34, rotateEncoder,rotateMotor);
+	public void initEncoder(){
+	//	rotateEncoder.setDistancePerPulse(51.42);
+	
+	}
 	public void initDefaultCommand() {
-
+		
 	}
 
 	public void setAngleUp() {
 		
 		// Arbitrary number: more testing and research is required.
-		while (rotateEncoder.get() < 9001) {
+	//	while (rotateEncoder.get() < 9001) {
 			// Speed should depend on distance from goal which is acquired
 			// through the use of a PID loop/subsystem
 		//	rotateMotor.set(1);
-			return;
-		}
+	//		return;
+	//	}
 	
 	}
 
 	public void setAngleDown() {
 	
 		// Arbitrary number: more testing and research is required.
-		while (rotateEncoder.get() > -9001) {
-			// Speed should depend on distance from goal which is acquired
+	//	while (rotateEncoder.get() > -9001) {
+//			// Speed should depend on distance from goal which is acquired
 			// through the use of a PID loop/subsystem
 			
 			//rotateMotor.set(-1);
-			return;
-		}
+	//		return;
+	//	}
 	
 	}
 
